500be5476753c0c2d9db537f228b7e9c64744432
SalahaldenAhmed <salah7718815@gmail.com>
2026-02-24 16:51:05 +0700

    Servo motor control

--- TASK 2/Servo motor control/Servomotor _control/Src/main.c
+++ TASK 2/Servo motor control/Servomotor _control/Src/main.c
@@ -1,83 +1,100 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
 
-  /* Includes ------------------------------------------------------------------*/
-  #include "main.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 
-  /* Private includes ----------------------------------------------------------*/
-  /* USER CODE BEGIN Includes */
+/* USER CODE END Includes */
 
-  /* USER CODE END Includes */
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
 
-  /* Private typedef -----------------------------------------------------------*/
-  /* USER CODE BEGIN PTD */
+/* USER CODE END PTD */
 
-  /* USER CODE END PTD */
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
 
-  /* Private define ------------------------------------------------------------*/
-  /* USER CODE BEGIN PD */
+/* USER CODE END PD */
 
-  /* USER CODE END PD */
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
 
-  /* Private macro -------------------------------------------------------------*/
-  /* USER CODE BEGIN PM */
+/* USER CODE END PM */
 
-  /* USER CODE END PM */
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim4;
 
-  /* Private variables ---------------------------------------------------------*/
-  TIM_HandleTypeDef htim4;
+/* USER CODE BEGIN PV */
 
-  /* USER CODE BEGIN PV */
+/* USER CODE END PV */
 
-  /* USER CODE END PV */
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM4_Init(void);
+/* USER CODE BEGIN PFP */
 
-  /* Private function prototypes -----------------------------------------------*/
-  void SystemClock_Config(void);
-  static void MX_GPIO_Init(void);
-  static void MX_TIM4_Init(void);
-  /* USER CODE BEGIN PFP */
+/* USER CODE END PFP */
 
-  /* USER CODE END PFP */
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
 
-  /* Private user code ---------------------------------------------------------*/
-  /* USER CODE BEGIN 0 */
+/* USER CODE END 0 */
 
-  /* USER CODE END 0 */
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
 
-  /**
-    * @brief  The application entry point.
-    * @retval int
-    */
-  int main(void)
-  {
+  /* USER CODE BEGIN 1 */
 
-    /* USER CODE BEGIN 1 */
+  /* USER CODE END 1 */
 
-    /* USER CODE END 1 */
+  /* MCU Configuration--------------------------------------------------------*/
 
-    /* MCU Configuration--------------------------------------------------------*/
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-    HAL_Init();
+  /* USER CODE BEGIN Init */
 
-    /* USER CODE BEGIN Init */
+  /* USER CODE END Init */
 
-    /* USER CODE END Init */
+  /* Configure the system clock */
+  SystemClock_Config();
 
-    /* Configure the system clock */
-    SystemClock_Config();
+  /* USER CODE BEGIN SysInit */
 
-    /* USER CODE BEGIN SysInit */
+  /* USER CODE END SysInit */
 
-    /* USER CODE END SysInit */
-
-    /* Initialize all configured peripherals */
-    MX_GPIO_Init();
-    MX_TIM4_Init();
-    /* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM4_Init();
+  /* USER CODE BEGIN 2 */
     HAL_TIM_PWM_Start(&htim4 , TIM_CHANNEL_4);
-    /* USER CODE END 2 */
+  /* USER CODE END 2 */
 
-    /* Infinite loop */
-    /* USER CODE BEGIN WHILE */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
     int hartbit = 0;
     int last_press =1;
     int current_press =0;
